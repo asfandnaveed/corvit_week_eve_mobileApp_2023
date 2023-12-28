77e0f8df49988665753cf99fb33b708282b22a5a
asfand naveed <m.asfandnaveed@gmail.com>
2023-12-28 15:56:38 +0500

    changes

--- lib/shoes.dart
+++ lib/shoes.dart
@@ -6,6 +6,29 @@ class Shoes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xff101321),
+                  Color(0xff7586C9),
+                ],
+              )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text('Hello Corvit'),
+          ),
+        ],
+      ),
+    );
   }
 }
