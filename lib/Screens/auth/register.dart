6b83bd4e742c067e5a9d2704a4ed2610c3485a2f
asfand naveed <m.asfandnaveed@gmail.com>
2024-01-25 14:07:40 +0500

    changes

--- lib/Screens/auth/register.dart
+++ lib/Screens/auth/register.dart
@@ -1,6 +1,7 @@
 import 'package:duplicate/Screens/auth/login.dart';
 import 'package:duplicate/Screens/home/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,6 +16,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   TextEditingController inputEmail = TextEditingController();
   TextEditingController inputPassword = TextEditingController();
+  TextEditingController inputUsername = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,28 +35,49 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
               SizedBox(height: 30,),
               TextFormField(
+                controller: inputUsername,
+                decoration: InputDecoration(
+                  hintText: "Username"
+                ),
+              ),
+              TextFormField(
                 controller: inputEmail,
+                decoration: InputDecoration(
+                    hintText: "Email"
+                ),
               ),
               SizedBox(height: 20,),
               TextFormField(
                 controller: inputPassword,
                 obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Password"
+                ),
               ),
               SizedBox(height: 30,),
               ElevatedButton(
-                onPressed: (){
+                onPressed: () async {
                   final auth = FirebaseAuth.instance;
+                  final db = FirebaseDatabase.instance.ref();
 
-                  auth.createUserWithEmailAndPassword(
+                  await auth.createUserWithEmailAndPassword(
                       email: inputEmail.text,
                       password: inputPassword.text,
-                  );
+                  ).then((value){
 
-                  if(auth.currentUser !=null){
+                    db.child('Users').child(auth.currentUser!.uid).set({
+                      "email":inputEmail.text,
+                      "password":inputPassword.text,
+                      "username":inputUsername.text,
+                    });
+
                     Navigator.push(context,
-                      MaterialPageRoute(builder: (context)=>HomePage())
+                        MaterialPageRoute(builder: (context)=>HomePage())
                     );
-                  }
+
+                  });
+
+                 
 
                 },
                 child: Text('Sign Up'),
